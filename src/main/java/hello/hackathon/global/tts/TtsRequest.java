bcae4f3626e80b7caf763766eb8b6184fd2c629b
hyungjun0312 <156741635+hyungjun0312@users.noreply.github.com>
2025-09-07 04:42:08 +0900

    Update TtsRequest.java

--- src/main/java/hello/hackathon/global/tts/TtsRequest.java
+++ src/main/java/hello/hackathon/global/tts/TtsRequest.java
@@ -1,10 +1,12 @@
 package hello.hackathon.global.tts;
 
+import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
+@Builder
 public class TtsRequest {
     private String text;
     private String gender;
